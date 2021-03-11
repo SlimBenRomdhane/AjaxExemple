5c4377e46bec2ae9f548858b3d84744fe0e14c34
SlimBenRomdhane <48137864+SlimBenRomdhane@users.noreply.github.com>
2021-03-11 17:06:27 +0100

    UpDate

--- AjaxExemple/Controllers/CustomerController.cs
+++ AjaxExemple/Controllers/CustomerController.cs
@@ -12,11 +12,11 @@ namespace AjaxExemple.Controllers
         List<CustomerModel> customers = new List<CustomerModel>();
         public CustomerController()
         {
-            customers.Add(new CustomerModel {Id =0, Name="Slouma", Age=35 });
-            customers.Add(new CustomerModel { Id = 1, Name = "Touta", Age = 28 });
+            customers.Add(new CustomerModel {Id =0, Name="Kimo", Age=35 });
+            customers.Add(new CustomerModel { Id = 1, Name = "Naira", Age = 28 });
             customers.Add(new CustomerModel { Id = 2, Name = "Ayouta", Age = 26 });
-            customers.Add(new CustomerModel { Id = 3, Name = "Tass", Age = 27 });
-            customers.Add(new CustomerModel { Id = 4, Name = "Maroua", Age = 26 });
+            customers.Add(new CustomerModel { Id = 3, Name = "Wass", Age = 27 });
+            customers.Add(new CustomerModel { Id = 4, Name = "Mari", Age = 26 });
         }
         public IActionResult Index()
         {
